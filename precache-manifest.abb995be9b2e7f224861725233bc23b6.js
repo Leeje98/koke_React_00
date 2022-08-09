55ff181ac34d605d36061b18ce0c9c34212af299
JE <wjd7dms7@naver.com>
2022-08-10 05:24:35 +0900

    Updates

---
+++ precache-manifest.abb995be9b2e7f224861725233bc23b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba88dabdfceb9afb12e9",
+    "url": "/koke_React/static/css/main.479d9ef0.chunk.css"
+  },
+  {
+    "revision": "ba88dabdfceb9afb12e9",
+    "url": "/koke_React/static/js/main.305aaab7.chunk.js"
+  },
+  {
+    "revision": "9ea0e2c8e43d690aa0f0",
+    "url": "/koke_React/static/js/runtime~main.eec8555e.js"
+  },
+  {
+    "revision": "e86bb7c404e300959f56",
+    "url": "/koke_React/static/css/2.756cd811.chunk.css"
+  },
+  {
+    "revision": "e86bb7c404e300959f56",
+    "url": "/koke_React/static/js/2.95e9a3ed.chunk.js"
+  },
+  {
+    "revision": "1972f29574012851c652",
+    "url": "/koke_React/static/js/3.1749d0fc.chunk.js"
+  },
+  {
+    "revision": "48461edd33c95abd02ffb1132cc0a39f",
+    "url": "/koke_React/static/media/main_visual.48461edd.jpeg"
+  },
+  {
+    "revision": "a171e5878440f3bba748d61a9d495312",
+    "url": "/koke_React/static/media/main_banner.a171e587.jpg"
+  },
+  {
+    "revision": "c4c5d388bab59735ae311608c72e8a6f",
+    "url": "/koke_React/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/koke_React/precache-manifest.abb995be9b2e7f224861725233bc23b6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/koke_React/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
